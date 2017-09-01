71b8e8a09fac1d4b56ce53bfdf1dd9ee6694551b
Alejandro Alcalde <algui91@gmail.com>
2017-09-01 09:05:22 +0200

    Updated backup.sh

--- home/bin/backup.sh
+++ home/bin/backup.sh
@@ -4,4 +4,8 @@
 ## Params:
 ##   $1: Backup destination: eg: /media/drive
 
+# Delete cache / temp files before backing up
+
+rm -rfv ~/.cache ~/.local/share/Trash ~/.thumbnails
+
 rsync -av --delete --exclude '/home/hkr/ssd2/usr' --exclude '/home/hkr/ssd2/portage' ~ /etc/portage "$1"
