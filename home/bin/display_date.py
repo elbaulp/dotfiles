f7daba72c6b121568145c81fdb3f57619ff7e686
Alejandro Alcalde <algui91@gmail.com>
2020-04-07 19:43:59 +0200

    Update display.py
    Signed-off-by: Alejandro Alcalde <algui91@gmail.com>

--- home/bin/display_date.py
+++ home/bin/display_date.py
@@ -4,10 +4,11 @@
 # Display date in dwm's status line
 
 
-from Xlib.display import Display
-from datetime import datetime
 import subprocess
 import time
+from datetime import datetime
+
+from Xlib.display import Display
 
 r = Display().screen().root
 
@@ -16,8 +17,8 @@ while True:
         "upower --show-info $(upower --enumerate | grep BAT) | grep percentage | grep -o \"[0-9]\+\" | head -1",
         shell = True,
         stderr = subprocess.STDOUT)[:-1]
-    currtime = datetime.now().strftime(u' CW %V, %a %b %e %T %Y ')
-    status_line = " Bat: {0}% {1}".format(battery, currtime)
+    currtime = datetime.now().strftime(' CW %V, %a %b %e %T %Y ')
+    status_line = f" Bat: {battery}% {currtime}"
 
     r.set_wm_name(status_line)
     r.get_wm_name()
