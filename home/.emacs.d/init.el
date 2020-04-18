b4982fe78dddc6de0ff048b44156166f37045211
Alejandro Alcalde <algui91@gmail.com>
2020-04-18 07:53:31 +0200

    [emacs] Change prompt color for haskell

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -584,6 +584,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(haskell-interactive-face-prompt ((t (:inherit font-lock-function-name-face :foreground "#ff8700"))))
  '(term-color-white ((t (:background "#263238" :foreground "light green")))))
 (put 'set-goal-column 'disabled nil)
 (provide 'init)
