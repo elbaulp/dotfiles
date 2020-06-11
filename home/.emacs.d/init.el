b6b36bbb011eaa1013cb529909282b24ab3bb391
Alejandro Alcalde <alejandro.alcalde@moia.io>
2020-06-11 21:15:54 +0200

    [emacs] Change org-roam dir

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -227,7 +227,7 @@
   :hook
   (after-init . org-roam-mode)
   :custom
-  (org-roam-directory "~/org-roam")
+  (org-roam-directory "~/Nextcloud/org-roam")
   :bind (:map org-roam-mode-map
               (("C-c n l" . org-roam)
                ("C-c n f" . org-roam-find-file)
